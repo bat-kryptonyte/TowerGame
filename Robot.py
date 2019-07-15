ebb3821b9a9f5f608d1c5036e76947ffa138a564
jjdow75 <13918831764@163.com>
2019-07-15 14:14:30 -0400

    abstract_robot

--- Robot.py
+++ Robot.py
@@ -1,3 +1,16 @@
 class abstract_robot(object):
-    def get_move(num, tower):
+    def choice(num, tower):
+        '''Returns a boolean indicating whether to choose to use the first card or not. '''
+        pass
+
+    def chear_choice(num, tower, ptower, discard_pile, deck):
+        '''Decide when knowing all decks. '''
+        pass
+
+    def get_normal_move(num, tower):
+        '''Returns the number to be replaced when choosing the first card. '''
+        pass
+    
+    def get_question_move(num, tower):
+        '''Returns the number to be replaced when choosing the question card, -1 for discard. '''
         pass

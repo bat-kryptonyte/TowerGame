f8b719318e1a3ec4c49a3779a456db6ff3948c21
jjdow75 <13918831764@163.com>
2019-07-17 10:14:33 -0400

    robot1

--- robot1.py
+++ robot1.py
@@ -1,5 +1,6 @@
 from Player import player
-from anytree import Node, RenderTree, search
+from numba import jit
+from math import inf
 class robot1(player):
     def choice(num, tower, ptower, discard_pile, deck):
         pass
@@ -7,21 +8,55 @@ class robot1(player):
         if player:
             best = []
 
-tree = []
-namelist = list(range(21 ** 5))
+class node(object):
 
-def get_available_moves(tower, discard_pile, deck):
+    def __init__(self, pt, rt, discard_pile, deck, player):
+        self.player = player
+        self.lst = [discard_pile, deck]
+        self.rt = rt
+        self.pt = pt
+
+    def get_rt(self):
+        return self.rt
+    
+    def get_pt(self):
+        return self.pt
+
+    def get_discard(self):
+        return self.lst[0]
+
+    def get_deck(self):
+        return self.lst[1]
+    
+    def is_robot(self):
+        return self.player
+
+def generate_subnode(n):
         '''Returns [[new_tower, new_discard_pile, new_deck], [], []...]'''
-        if len(deck) > 0:
-            result = [[tower, [deck[0]] + discard_pile, deck[1:]]]
+        discard_pile = n.get_discard()
+        deck = n.get_deck()
+        if not n.is_robot():
+            tower = n.get_pt()
+            rt = n.get_rt()
+            if len(deck) == 0:
+                return []
+            result = [node(tower[:], rt, [deck[0]] + discard_pile[:], deck[1:], not n.is_robot())]
+            for i in range(len(tower)):
+                result.append(node(tower[0:i] + [discard_pile[0]] + tower[i + 1:], rt, [tower[i]] + discard_pile[1:], deck, not n.is_robot()))
+                if len(deck) > 0:
+                    result.append(node(tower[0:i] + [deck[0]] + tower[i + 1:], rt, [tower[i]] + discard_pile, deck[1:], not n.is_robot()))
         else:
-            result = []
-        for i in range(len(tower)):
-            result.append([tower[0:i] + [discard_pile[0]] + tower[i + 1:], [tower[i]] + discard_pile[1:], deck])
-            if len(deck) > 0:
-                result.append([tower[0:i] + [deck[0]] + tower[i + 1:], [tower[i]] + discard_pile, deck[1:]])
+            tower = n.get_rt()
+            pt = n.get_pt()
+            if len(deck) == 0:
+                return []
+            result = [node(pt, tower[:], [deck[0]] + discard_pile[:], deck[1:], not n.is_robot())]
+            for i in range(len(tower)):
+                result.append(node(pt, tower[0:i] + [discard_pile[0]] + tower[i + 1:], [tower[i]] + discard_pile[1:], deck, not n.is_robot()))
+                if len(deck) > 0:
+                    result.append(node(pt, tower[0:i] + [deck[0]] + tower[i + 1:], [tower[i]] + discard_pile, deck[1:], not n.is_robot()))
         return result
-
+'''
 def generate_tree(pt, rt, discard_pile, deck, node, player, p):
     if player:
         p_node = Node(str(namelist.pop(0)), parent = p, move = [rt, discard_pile, deck])
@@ -29,7 +64,6 @@ def generate_tree(pt, rt, discard_pile, deck, node, player, p):
         if node == 0:
             return
         lst = get_available_moves(rt, discard_pile, deck)
-        print(lst)
         for m in lst:
             generate_tree(pt, m[0], m[1], m[2], node - 1, False, p_node)
     else:
@@ -40,20 +74,62 @@ def generate_tree(pt, rt, discard_pile, deck, node, player, p):
         lst = get_available_moves(pt, discard_pile, deck)
         for m in lst:
             generate_tree(m[0], rt, m[1], m[2], node - 1, True, p_node)
+'''
+def count_order(playerTower):
+	temp = []
+	count = 0
+	max = 0
+	alst = []
+	lst = [playerTower[0]]
+	i = 1
+	for y in range(len(playerTower) - 1):
+		if(playerTower[y] > playerTower[y + 1]):
+			lst.append(playerTower[y])
+			temp = lst
+			alst.append(temp)
+			lst = []
+		else:
+			lst.append(playerTower[y])
+	for x in range(len(alst) - 1):
+		if(len(alst[x]) < len(alst[x + 1])):
+			max = len(alst[x + 1])
+	return (max + 1)/10 * 100
 
+def evaluate(n):
+    playerTower = n.get_pt()[:]
+    vikingTower = n.get_rt()[:]
+    x = count_order(playerTower)
+    y = count_order(vikingTower)
+    return y - x
 
-def get_score_list(lst, depth, rt, pt, evaluate):
+def minimax(current_node, depth, player, evaluate):
     if depth == 0:
-        lst.append(evaluate(rt, pt))
-        return
+        return evaluate(current_node)
+    if player:
+        value = -inf
+        for n in generate_subnode(current_node):
+            value = max(value, minimax(n, depth - 1, False, evaluate))
+        return value
     else:
-        pass
+        value = inf
+        for n in generate_subnode(current_node):
+            value = min(value, minimax(n, depth - 1, True, evaluate))
+        return value
 
-rt = [46, 29, 5, 36, 44, 23, 42, 40, 26, 48]
-discard = [24]
-deck = [10, 22, 21, 39, 43, 14, 6, 35, 41, 11, 4, 28, 32, 33, 18, 13, 37, 16, 47, 38, 50, 3, 25, 31, 2, 49, 1, 20, 17]
-pt = [12, 34, 7, 8, 30, 45, 9, 27, 19, 15]
+def main():
+    rt = [20, 29, 30, 36, 44, 23, 42, 40, 26, 48]
+    discard = [24]
+    deck = [10, 22, 21, 39, 43, 14, 6, 35, 41, 11, 4, 28, 32, 33, 18, 13, 37, 16, 47, 38, 50, 3, 25, 31, 2, 49, 1, 20, 17]
+    pt = [12, 34, 7, 8, 30, 45, 9, 27, 19, 15]
+    nd = node(pt, rt, discard, deck, True)
+    print(evaluate(nd))
+    #print(minimax(nd, 3, False, evaluate))
+
 #print(get_available_moves(rt, discard, deck))
-generate_tree(pt, rt, discard, deck, 2, True, None)
-t = RenderTree(tree[0])
-print(t)
+'''
+generate_tree(pt, rt, discard, deck, 4, True, None)
+print("finish generating")
+#t = RenderTree(tree[0])
+print(len(tree))
+'''
+main()
